2af27835dc062d7495ccba62e6f9721bb369fe78
freaklucas <lucas.oliveira.freak@gmail.com>
2022-02-17 23:23:03 -0300

    alinhando componentes

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
+
 import Home from "../views/Home.vue";
+import Counter from "../components/Counter.vue";
+import Employees from "../components/Employees.vue";
+import UserList from "../components/UserList.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
+  },
+  {
+    path: "/counter",
+    name: "Counter",
+    component: Counter,
+  },
+  {
+    path: "/employees",
+    name: "Employees",
+    component: Employees,
+  },
+  {
+    path: "/users",
+    name: "UserList",
+    component: UserList,
   },
 ];
 
