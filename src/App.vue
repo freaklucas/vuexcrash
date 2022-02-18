e458d1a61991406c7b7845174927ad560fc47da0
freaklucas <lucas.oliveira.freak@gmail.com>
2022-02-17 23:26:08 -0300

    sequencia de components ok

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
-  <h1>App</h1>
+  <Navbar />
+  <router-view />
 </template>
+<script>
+import Navbar from "@/components/Navbar.vue";
+
+export default {
+  components: { Navbar },
+};
+</script>
+
 
 <style>
 @import "style.css";
