0f8dada16fd1ff77ac05bc1707543b71519292ae
freaklucas <lucas.oliveira.freak@gmail.com>
2022-03-01 23:08:23 -0300

    config modules state

--- src/store/index.js
+++ src/store/index.js
@@ -2,17 +2,20 @@ import { createStore } from "vuex";
 
 import counterModule from "./modules/counter.module";
 import employeeModule from "./modules/employee.module";
+import usersModule from "./modules/users.module";
 
 export default createStore({
   state: {
     counterState: counterModule.state,
     employeeState: employeeModule.state,
+    usersState: usersModule.state,
   },
   mutations: {},
   actions: {},
   modules: {
     counterModule,
     employeeModule,
+    usersModule,
   },
   getters: {
     getCounterState: function (state) {
