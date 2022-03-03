2337573b000e87eb3c6e71606adb3ce1141dbe50
freaklucas <lucas.oliveira.freak@gmail.com>
2022-03-02 23:29:35 -0300

    implementando carregamento e error

--- src/store/modules/users.module.js
+++ src/store/modules/users.module.js
@@ -1,6 +1,24 @@
 export default {
   namespaced: true,
-  state: {},
-  mutations: {},
+  state: {
+    userList: {
+      loading: false,
+      users: [],
+      errorMessage: null,
+    },
+  },
+  mutations: {
+    GET_USERS_REQUEST: function (state) {
+      state.userList.loading = true;
+    },
+    GET_USERS_SUCCESS: function (state, payload) {
+      state.userList.loading = false;
+      state.userList.users = payload.users;
+    },
+    GET_USERS_FAILED: function (state, payload) {
+      state.userList.loading = false;
+      state.userList.errorMessage = payload.error;
+    },
+  },
   actions: {},
 };
